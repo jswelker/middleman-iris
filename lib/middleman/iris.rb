82608d39bb872e27d67a6a36ba1221163da4027d
JCKL Automation <library@ucmo.edu>
2018-02-12 15:23:44 -0600

    Initial commit

---
+++ lib/middleman/iris.rb
@@ -0,0 +1,7 @@
+require "middleman/iris/version"
+
+module Middleman
+  module Iris
+    # Your code goes here...
+  end
+end
---
+++ lib/middleman/iris/version.rb
@@ -0,0 +1,5 @@
+module Middleman
+  module Iris
+    VERSION = "0.1.0"
+  end
+end
